2408fffb5aaa0554fb44b45addd80f3a81482f59
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-05-05 18:39:30 +0100

    Update removeTask file

--- src/modules/removeTask.js
+++ src/modules/removeTask.js
@@ -1,10 +1,8 @@
-import showTasks from './showTasks.js';
-
 const removeTask = (arr, index) => {
   arr.splice(index, 1);
   for (let i = index; i < arr.length; i += 1) {
-    arr[i].index = i;
+    arr[i].index = i + 1;
   }
-  showTasks(arr);
 };
+
 export default removeTask;
